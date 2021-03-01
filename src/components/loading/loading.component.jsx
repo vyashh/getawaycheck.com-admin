64c7bd6a0bc508a36db55241956bcf00e40129d1
Vyash Bhawan <vyash@shogundev.com>
2021-03-01 19:19:48 +0100

    added loading animation

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import LoadingAnimation from "../../assets/img/loading.svg";
+import "./loading.styles.scss";
 
 export default function Loading() {
   return (
-    <div>
-      <h1>Loading</h1>
+    <div className="loading">
+      <img src={LoadingAnimation} alt="loading animation" />
     </div>
   );
 }
